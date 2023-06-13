602a6fc3593b6e9ea1350bffad93363d3661578d
Angel Cruz <77132950+xXAngel-15Xx@users.noreply.github.com>
2023-06-12 19:52:02 -0500

    update login
    link para crear cuenta en el login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -76,6 +76,25 @@ class LoginPage extends StatelessWidget {
                     style: btnPurple(),
                     child: const Text('Iniciar Sesión'),
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        '¿No Tienes Cuenta?',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextButton(
+                        onPressed: () =>
+                            Navigator.pushNamed(context, 'register'),
+                        child: const Text(
+                          'Crear Cuenta',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
